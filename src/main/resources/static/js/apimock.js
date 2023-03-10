5d61cc9651dc29c5726f229db598b136ce96bb1f
DanielOchoa <danochoa1412@gmail.com>
2023-03-09 21:12:29 -0500

    Casi terminado

--- src/main/resources/static/js/apimock.js
+++ src/main/resources/static/js/apimock.js
@@ -9,7 +9,7 @@ apimock = (function () {
 	mockdata["maryweyland"]=[{author:"maryweyland","points":[{"x":140,"y":140},{"x":115,"y":115}],"name":"house2"},
 	 {author:"maryweyland","points":[{"x":140,"y":140},{"x":115,"y":115}],"name":"gear2"}];
      mockdata["johncito"]=[{author:"johncito","points":[{"x":0,"y":0},{"x":0,"y":0},{"x":1,"y":1},{"x":0,"y":0}],"name":"thepaint"},
-	 {author:"johncito","points":[{"x":0,"y":0},{"x":0,"y":0},{"x":1,"y":1},{"x":0,"y":0}],"name":"thepaintV2"}];
+	 {author:"johncito","points":[{"x":100,"y":100},{"x":400,"y":100},{"x":400,"y":400},{"x":100,"y":400},{"x":100,"y":100}],"name":"thepaintV2"}];
     mockdata["john"]=[{author:"john","points":[{"x":0,"y":0},{"x":0,"y":0},{"x":1,"y":1},{"x":0,"y":0}],"name":"thepaint"}];
     mockdata["Ibai"]=[{author:"Ibai","points":[{"x":140,"y":140},{"x":115,"y":115}],"name":"LaVeladaDelAño"}];
 
--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -5,25 +5,63 @@ let app = (function (api) {
     let _blueprints = [];
     let publicFunctions = {};
 
-    let _bpsToObject = (data) => {
-        objData = data.map((x) => (
+    let _renderSearch = (data) => {
+        $(document).ready(() => {
+            objData = _dataToObject(data);
+            _renderTable(objData, data);
+            _calculateTotalPoints(objData);
+            _blueprints = objData;
+        });
+    }
+
+    let _dataToObject = (data) => {
+        let obj = data.map((x) => (
             {
                 name: x.name,
                 pointsNum : x.points.length
             }
         ));
+        return obj;
+    }
+
+    let _renderTable = (objData, data) => {
         $("#blueprints tbody").text("");
         objData.map((x) => {
-            $(document).ready(() => {
-                let markup = `<tr><td>${x.name}</td><td>${x.pointsNum}</td></tr>`;
-                $("#blueprints tbody").append(markup);
-            });
+            let markup = `
+            <tr>
+                <td>${x.name}</td>
+                <td>${x.pointsNum}</td>
+                <td><button onclick="app.drawBlueprint('${data[0].author}', '${x.name}')">Open</button></td>
+            </tr>`;
+            $("#blueprints tbody").append(markup);
         });
+    }
+
+    let _calculateTotalPoints = (objData) => {
         let totalPoints = objData.reduce((total, i) => total + i.pointsNum, 0);
+        $("#total-points").html(totalPoints);
+    }
+
+    let _renderBlueprint = (data) => {
         $(document).ready(() => {
-            $("#total-points").html(totalPoints);
+            _drawBlueprint(data);
+            // PREGUTNAR
+            $("#blueprint-name").html(data.name);
         });
-        _blueprints = objData;
+    }
+
+    let _drawBlueprint = (data) => {
+        let points = data.points;
+        let canvas = $("#blueprints-canvas")[0];
+        let ctx = canvas.getContext("2d");
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.beginPath();
+        ctx.moveTo(points[0].x, points[0].y);
+        for (let i = 1; i < points.length; i++) {
+            const point = points[i];
+            ctx.lineTo(point.x, point.y);
+        }
+        ctx.stroke();
     }
 
     publicFunctions.setName = function (newName) {
@@ -31,13 +69,18 @@ let app = (function (api) {
     }
 
     publicFunctions.updateBlueprints = function (authorName) {
-        api.getBlueprintsByAuthor(authorName, _bpsToObject);
+        api.getBlueprintsByAuthor(authorName, _renderSearch);
+    }
+    publicFunctions.drawBlueprint = function (authorName, bpName) {
+        console.log(authorName);
+        api.getBlueprintsByNameAndAuthor(authorName, bpName, _renderBlueprint);
     }
 
     return publicFunctions;
 
 })(apimock);
 
+app.drawBlueprint("Ibai", "LaVeladaDelAño");
 
 /*
 Example of use:
